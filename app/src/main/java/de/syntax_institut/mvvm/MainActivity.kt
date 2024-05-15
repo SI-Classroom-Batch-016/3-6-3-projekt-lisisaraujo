204d16cab426505de9d5e257f5e17251bcf7f639
Lisis Ruschel <laruschel@proton.me>
2024-05-15 12:48:22 +0200

    commented out mongoDB config. Maps loads again, still have the scroll problem.

--- app/src/main/java/de/syntax_institut/mvvm/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/mvvm/MainActivity.kt
@@ -2,28 +2,28 @@ package de.syntax_institut.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
-import com.mongodb.kotlin.client.coroutine.MongoClient
-import com.mongodb.kotlin.client.coroutine.MongoDatabase
-import de.syntax_institut.mvvm.data.model.Location
-import org.bson.Document
+//import android.util.Log
+//import com.mongodb.ConnectionString
+//import com.mongodb.MongoClientSettings
+//import com.mongodb.kotlin.client.coroutine.MongoClient
+//import com.mongodb.kotlin.client.coroutine.MongoDatabase
+//import de.syntax_institut.mvvm.data.model.Location
+//import org.bson.Document
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val connectionString =
-            ConnectionString("mongodb+srv://laruschel:L14vE6R0fb5RvHWF@cluster0.cwpjgeg.mongodb.net/queer_map")
-        val settings = MongoClientSettings.builder().applyConnectionString(connectionString).build()
-        val mongoClient = MongoClient.create(settings)
-
-        val database: MongoDatabase = mongoClient.getDatabase("q4q-database")
-
-        val locations = database.getCollection<String>("locations")
-        Log.d("Main", locations.toString())
+//
+//        val connectionString =
+//            ConnectionString("mongodb+srv://laruschel:L14vE6R0fb5RvHWF@cluster0.cwpjgeg.mongodb.net/queer_map")
+//        val settings = MongoClientSettings.builder().applyConnectionString(connectionString).build()
+//        val mongoClient = MongoClient.create(settings)
+//
+//        val database: MongoDatabase = mongoClient.getDatabase("q4q-database")
+//
+//        val locations = database.getCollection<String>("locations")
+//        Log.d("Main", locations.toString())
 
 
     }
